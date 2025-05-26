703ff0d0467f73171d7d0fe08e7de0c3d0f79899
Astior02 <146308326+Astior02@users.noreply.github.com>
2025-05-26 15:02:40 -0300

    Arquivo base para o jogo

--- main.c
+++ main.c
@@ -1,35 +1,17 @@
 #include "raylib.h"
-
-/**
- *
- * Raylib (+ Raygui) VS Code Starter Template
- *
- * This assumes that raylib has been installed to the default location with the official installer.
- *
- * Hit F5 to Run/Debug the application.
- *
- * Happy Coding! :)
- *
- */
-
-// Comment or delete the following two lines if you don't need raygui in your project
 #define RAYGUI_IMPLEMENTATION
 #include "deps/raygui.h"
-
-// Include a dark style just to showcase how styles work
-#include "./deps/styles/style_dark.h" // raygui style: dark
+#include "./deps/styles/style_dark.h" 
 
 int main(void)
 {
+    const int tamanhoTelaX = 1920;
+    const int tamanhoTelaY = 1080;
+    InitWindow(tamanhoTelaX, tamanhoTelaY, "Project Chicken");
+    
+    
 
-    InitWindow(1920, 1080, "Project Chicken");
-
-    // Each style comes with its own style-loading-function.
-    // Just look in the corresponding styles .h file.
-    // When no style is loaded, the default white style will be used.
-    GuiLoadStyleDark();
-
-    int showMessageBox = false;
+    
 
     while (!WindowShouldClose())
     {
